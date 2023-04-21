4977609ffdba422677fa66bc29f9afcf477e9240
Will Tidwell <jwtidwell1@crimson.ua.edu>
2023-04-21 00:02:16 -0500

    started working on classes and interfaces.

---
+++ fighter.cs
@@ -0,0 +1,11 @@
+namespace piratespa4
+{
+    public class fighter : iattack
+    {
+        public int health {get; set;}
+        public int attackStrength {get; set;}
+        public bool isDebuffed {get; set;}
+        public int defensivePower {get; set;}
+
+    }
+}
---
+++ iattack.cs
@@ -0,0 +1,10 @@
+namespace piratespa4
+{
+    public interface iattack
+    {
+         public void AttackTurn()
+         {
+            System.Console.WriteLine("This character attacked");
+         }
+    }
+}
